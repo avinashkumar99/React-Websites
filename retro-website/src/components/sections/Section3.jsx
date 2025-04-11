b9d13a449c633fab862236db372125bea0385fc3
avinash <avinashhkumar99@gmail.com>
2025-04-11 23:42:51 +0530

    added section 4

--- retro-website/src/components/sections/Section3.jsx
+++ retro-website/src/components/sections/Section3.jsx
@@ -1,42 +1,62 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Section3 = () => {
   useEffect(() => {
-    //  gsap.to(".anchor-t" ,{
-    //   width: '100%',
-    //   duration:
-    //  })
+    gsap.fromTo(
+      ".ani-tv",
+      {
+        y: -50,
+        opacity: 0.1,
+      },
+      {
+        y: 0,
+        duration: 1,
+        opacity: 1,
+        delay: 0.5,
+        stagger: 0.7,
+        ease: "power1.in",
+        scrollTrigger: {
+          trigger: ".con-kv",
+          start: "top 80%",
+          end: "bottom bottom",
+          markers: true,
+          scrub: true,
+        },
+      }
+    );
   }, []);
   return (
-    <div className="h-auto md:h-screen w-full bg-black text-white">
+    <div className="con-kv h-auto md:h-screen w-full bg-black text-white">
       <div className="h-full w-full md:flex md:flex-col md:align-middle md:justify-center py-8 relative">
         {/* Relative position to parent  */}
         <div className="hidden md:block absolute right-4 top-4 group">
           <a href="#" className={`anchor-t lg:text-2xl md:text-xl relative`}>
             Learn more about our approach
           </a>
-          <span className="absolute w-0 h-[3px] bg-white bottom-0 left-0 transition-all duration-500 group-hover:w-full"></span>
+          <span className="absolute w-0 h-[3px] bg-white -bottom-2 left-0 transition-all duration-500 group-hover:w-full"></span>
         </div>
 
         {/* Normal flow  */}
-        <div className="h-auto md:w-[40%] w-[95%] text-3xl md:text-4xl lg:text-6xl mx-auto font-semibold mb-10">
+        <div className="ani-tv h-auto md:w-[40%] w-[95%] text-3xl md:text-4xl lg:text-6xl mx-auto font-semibold mb-10">
           Where Innovation Meets Identity: Shaping Brands for the Future.
         </div>
-        <p className="h-auto md:w-[40%] w-[95%] text-sm md:text-lg mx-auto my-6 text-[#c7bebd]">
+        <p className="ani-tv h-auto md:w-[40%] w-[95%] text-sm md:text-lg lg:text-xl mx-auto my-6 text-[#c7bebd]">
           At NEON Retrofuture, we craft more than just designs—we create
           immersive brand experiences that captivate and inspire. Our approach
           merges global influences with cutting-edge technology and a distinct
           retro-futuristic vision, leading to visually stunning and meaningful
           identities.
         </p>
-        <p className="h-auto md:w-[40%] w-[95%] text-sm md:text-lg mx-auto text-[#c7bebd] mb-6">
+        <p className="ani-tv h-auto md:w-[40%] w-[95%] text-sm md:text-lg lg:text-xl mx-auto text-[#c7bebd] mb-6">
           We transform innovative concepts into designs that not only resonate
           across diverse cultures but also endure over time. Discover how our
           blend of creativity and expertise shapes unforgettable brand stories
           and forges connections that last.
         </p>
-        <div className="md:hidden p-4 mb-4 group ">
+        <div className="md:hidden p-4 mb-4 group">
           <a href="#" className="anchor-t relative">
             Learn more about our approach
           </a>
--- retro-website/src/components/sections/Section4.jsx
+++ retro-website/src/components/sections/Section4.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 const Section4 = () => {
-  return <div className="h-screen w-full bg-purple-600">Section4</div>;
+  useEffect(() => {
+    gsap.fromTo(
+      ".ani-sec4",
+      {
+        y: 50,
+        opacity: 0,
+        duration: 1,
+      },
+      {
+        y: -100,
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".ani-sec4m",
+          start: "top 80%",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+  return (
+    <div className="ani-sec4m h-screen w-full flex flex-col lg:text-[10rem] xl:text-[12rem] 2xl:text-[20rem] md:text-[8rem] text-[4rem] bg-white font-extrabold p-4">
+      <div className="ani-sec4 h-auto w-full ani-sec4 text-center">
+        FEATURED
+      </div>
+      <div className="ani-sec4 h-auto w-full ani-sec4 text-center">WORK</div>
+    </div>
+  );
 };
 
 export default Section4;
